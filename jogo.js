bc165e198a401eec03e972f2431b77e6376b98f0
Pires-Felipe <felipe.miranda.pires@escola.pr.gov.br>
2022-04-20 15:51:47 -0300

    primeiro commit

--- jogo.js
+++ jogo.js
@@ -1,8 +1,19 @@
+let imgDaEstrada;
+let imgDoAtor;
+let imgDoCarro;
+
+function preload (){
+   imgDaEstrada = loadImage("img/estrada.png");
+   imgDoAtor = loadImage("img/ator-1")
+   imgDoCarro =loadImage("img/carro-1.png")
+}
+
 
 function setup() {
    createCanvas(500,400)
 }
 
 function draw() {
-   background(220)
+   background(imgDaEstrada);
+   Image(imgDoATor, 100, 460);
 }
